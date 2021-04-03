dbe9ef6645a50f24ba9c68f0887a9da8c1fddd64
Anton <antonpedersen9@gmail.com>
2021-04-03 13:17:07 +0200

    Cat lsaer v3

--- main.ino
+++ main.ino
@@ -6,185 +6,190 @@
 #include <Servo.h>
 #include <math.h>
 
-// Instantiating two servos
-Servo xServo;
-Servo yServo;
+struct Point {
+	int x;
+	int y;
+};
 
+bool debug = false;
 
 // Variables
-int startX = 90; // Start degree left to right, 90 degrees is center
-int startY = 20; // Start degree near to far, 0 is nearest
-
+byte laserPin = 13;
+byte xServoPin = 6;
+byte yServoPin = 9;
 int minX = 70;	 // Left bottom corner of triangle area
 int maxX = 110;  // Right bottom corner of triangle area
 int minY = 0; 	 // Base of triangle area
 int maxY = 45; 	 // Top of triangle area
 
+int startX = (minX+((maxX - minX)/2));
+int startY = maxY/2;
+
 int movementTime = 1000; // Total movement time (ms)
 int movementSteps = 10;	 // Number of steps to move during movement_time
 
-
-void attachServos() {
-	xServo.attach(6); // attaches the x servo on pin 6 to the servo object
-	yServo.attach(9); // attaches the y servo on pin 9 to the servo object
-}
-
-void detachServos() {
-	xServo.detach();
-	yServo.detach();
-}
+// Create corners of triangle area
+Point pointA = { minX, minY };
+Point pointB = { startX, maxY};
+Point pointC = { maxX, minY };
 
 
-// Helper function
+// Laser helper class
+class Servos {
+	private: 
+		Servo xServo;
+		Servo yServo;
+		byte lPin;
+		byte xPin;
+		byte yPin;
+
+	public:
+		Servos(byte p1, byte p2, byte p3 ) {
+			lPin = p1;
+			xPin = p2;
+			yPin = p3;
+
+			// Attach laser
+			pinMode(lPin, OUTPUT);
+		}
+
+		void activateLaser() {
+			digitalWrite(lPin, HIGH);
+		}
+
+		void deactivateLaser() {
+			digitalWrite(lPin, LOW);
+		}
+
+		void attach() {
+			xServo.attach(xPin);
+			yServo.attach(yPin);
+		}
+
+		void detach() {
+			xServo.detach();
+			yServo.detach();
+		}
+
+		void pointAt(Point p) {
+			xServo.write(p.x);
+			yServo.write(p.y);
+		}
+};
+
+// Declare laser
+Point laserPosition = { startX, startY };
+Servos Laser(laserPin, xServoPin, yServoPin);
+
+
+// Helper function for pointInTriangle
 float sign (Point p1, Point p2, Point p3) {
     return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
 }
 
-// Helper function
-bool pointInTriangle (Point pt, Point v1, Point v2, Point v3) {
-    float d1, d2, d3;
-    bool has_neg, has_pos;
+// Test if point is inside defined triangle area
+bool pointInTriangle (Point point) {
+    bool hasNeg, hasPos;
+    float d1 = sign(point, pointA, pointB);
+    float d2 = sign(point, pointB, pointC);
+    float d3 = sign(point, pointC, pointA);
 
-    d1 = sign(pt, v1, v2);
-    d2 = sign(pt, v2, v3);
-    d3 = sign(pt, v3, v1);
+    hasNeg = (d1 < 0) || (d2 < 0) || (d3 < 0);
+    hasPos = (d1 > 0) || (d2 > 0) || (d3 > 0);
 
-    has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
-    has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
+    return !(hasNeg && hasPos);
+}
 
-    return !(has_neg && has_pos);
+// Generate points inside defined area until one is inside the allowed triangle
+// The points pointA, pointB, pointC define the triangle
+Point getValidTarget() {
+	Point position;
+	bool isInTriangle = false;
+
+	while ( !isInTriangle ) {
+		position.x = random(minX, (maxX + 1));
+		position.y = random(minY, (maxY + 1));
+		isInTriangle = pointInTriangle(position);
+	}
+
+	return position;
 }
 
 
-// Point class
-class Point {
-	public:
-		float x, y;
-		float speedX, speedY;
-		float targetDistance;
-		int targetX, targetY;
-		bool active = false;
+// Get distance between two points
+int getDistance(Point p1, Point p2) {
+	return sqrt(pow(p2.x - p1.x, 2) + pow(p2.y- p1.y, 2) * 1.0);
+}
 
-		// Constructor
-		Point(int initX, int initY) {
-			x = initX;
-			y = initY;
-		}
-
-	// Update a targetPosition to a new point inside the defined triangle area
-	void updateValidTargetPosition() {
-		Point targetPoint;
-		bool isInTriangle = false;
-
-		while ( !isInTriangle ) {
-			targetPoint.x = random(minX, (maxX + 1));
-			targetPoint.y = random(minY, (maxY + 1));
-			isInTriangle = pointInTriangle(targetPoint, p1, p2, p3);
-		}
-
-		targetX = targetPoint.x;
-		targetY = targetPoint.y;
+// Get the time to wait between loops
+// The dealy will increase exponentially depending on point distances.
+float getDelayTimer(int distance) {
+	float factor = 1 + (3 * pow((distance/100), 5));
+	float timer = 1000 * factor;
+	if ( timer < 10000 ) {
+		return timer;
 	}
-
-	// Update distance between current point and target point
-	void updateCurrentToTargetDistance() {
-    	targetDistance = sqrt(pow(x - targetX, 2) + pow(y - targetY, 2) * 1.0);
-	}
-
-	// Update distance per movement itteration
-	void updateSpeed() {
-		speedX = (targetX - x) / movementSteps;
-		speedY = (targetY - y) / movementSteps;
-	}
-
-	// The further the distance between current and target point, increase delay exponentially
-	float getIdleTimer() {
-		float factor = 1 + (3 * pow((targetDistance/100), 5));
-		float timer = 1000 * factor;
-		if ( timer < 10000 ) {
-			return timer;
-		}
-		return 10000;
-	}
-
-	void deactivate() {
-		active = false;
-		detachServos();
-	}
-
-	// Initialize continous movment
-	void activate() {
-		active = true;
-		attachServos();
-		updateValidTargetPosition();
-		updateCurrentToTargetDistance();
-		updateSpeed();
-		move(1);
-	}
-
-	// Move towards target location
-	void move(int i) {
-		x = x + speedX;
-		y = y + speedY;
-
-		// Move servo
-		xServo.write(x);
-		yServo.write(y);
-
-		if ( i < movementSteps ) {
-			delay( movementTime/movementSteps );
-			move(i + 1);
-
-		} else {
-			idle();
-		}
-	}
-
-	// Set to idle
-	void idle() {
-
-		// Detach to remove buzzing sound from the servo
-		detachServos();
-
-		delay( getIdleTimer() );
-		if ( active ) {
-			activate();
-		}
-	}
-};
-
-
-// Create corners of triangle area
-Point p1(minX, minY);
-Point p2((minX+((maxX - minX)/2)), maxY);
-Point p3(maxX, minY);
-
-// Create point for laser pointer
-Point laserPoint(startX, startY);
+	return 10000;
+}
 
 
 // Runs on start
 void setup() {
-	
+
 	// Initialize serial communication at 9600 bits per second:
 	Serial.begin(9600);
 
-	attachServos();
-
-	// Start laser
-	pinMode (13, OUTPUT);
-	digitalWrite (13, HIGH); // switch on the laser
-	//digitalWrite (13, LOW); // switch off the laser
-
-	// Set servo start-pos
-	xServo.write(startX);
-	yServo.write(startY);
+	// Setup laser
+	Laser.attach();
+	Laser.activateLaser();
+	Laser.pointAt(laserPosition);
 }
-
 
 // Runs continuously after setup() is done
 void loop() {
-	if ( !laserPoint.active ) {
-		laserPoint.activate();
+
+	if ( debug ) {
+
+		Serial.println("Debug");
+
+	} else { 
+		Laser.attach();
+
+		// Get a new valid target for the laser
+		Point newTarget = getValidTarget();
+		Serial.print("Target acquired: x:");
+		Serial.print(newTarget.x);
+		Serial.print(", y: ");
+		Serial.print(newTarget.y);
+		Serial.println();
+
+		// Get distance between new and current target
+		int distance = getDistance(laserPosition, newTarget);
+
+		// Move the laser a small step at a time
+		int xStep = (newTarget.x - laserPosition.x) / movementSteps;
+		int yStep = (newTarget.y - laserPosition.y) / movementSteps;
+		Point laserTarget = { laserPosition.x, laserPosition.y };
+		for (int i = 0; i < movementSteps; i++) {
+			
+			laserTarget.x = laserTarget.x + xStep;
+			laserTarget.y = laserTarget.y + yStep;
+
+			Laser.pointAt(laserTarget);
+
+			delay( movementTime / movementSteps );
+		}
+
+		// Update the laser position for next loop
+		laserPosition = newTarget;
+
+		// Idle until next loop
+		Laser.detach(); // Detach to prevent flickering
+		float time = getDelayTimer(distance);
+		Serial.print("Idle for: ");
+		Serial.print(time);
+		Serial.print("ms");
+		Serial.println();
+		delay(time);
 	}
 }
